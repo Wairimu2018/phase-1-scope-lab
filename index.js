56bdf0c142137d1199f8a7fcd044b87822ce859f
Wairimu2018 <you@example.com>
2022-06-09 21:39:23 +0300

    Finished with Scope

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+function window (customerName){   
+    var customerName= function() {
+      return customerName;          
+    }
+
+    return customerName;
+}
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+      }
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+      }
+const leastFavoriteCustomer = 'Madison'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Madison'
+}
